0089bbae9c87d850c50f5b5d75b3e4e6f7d19d73
alexey.sokolov <alexey.sokolov@exactrosystems.com>
2022-03-30 15:42:02 +0300

    solving problem with checks

--- ci/th2_infra_test.go
+++ ci/th2_infra_test.go
@@ -10,27 +10,24 @@ import (
 	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/logger"
-
-	"net/http"
-    "moul.io/http2curl"
 )
 
 const (
-	defaultSchemaNamespace = "th2-schema"
-	serviceNamespace       = "service"
-	monitoringNamespace    = "monitoring"
-	rabbitmqPod            = "rabbitmq-0"
-	rabbitmqSvc            = "rabbitmq-discovery"
-	rabbitmqUser           = "th2"
-	rabbitmqPassword       = "test"
-	cassandraPod           = "cassandra-0"
-	dataProviderSvc        = "rpt-data-provider"
-	reportViewerSvc        = "rpt-data-viewer"
-	infraMgrSvc            = "infra-mgr"
-	infraEditorSvc         = "infra-editor"
-	dashboardSvc           = "th2-infra-dashboard"
-	retries                = 10
-	timeout                = 5 * time.Second
+	defaultSchemaNamespace    = "th2-schema"
+	defaultServiceNamespace   = "service"
+	monitoringNamespace       = "monitoring"
+	rabbitmqPod               = "rabbitmq-0"
+	rabbitmqSvc               = "rabbitmq-discovery"
+	rabbitmqUser              = "th2"
+	rabbitmqPassword          = "test"
+	cassandraPod              = "cassandra-0"
+	dataProviderSvc           = "rpt-data-provider"
+	reportViewerSvc           = "rpt-data-viewer"
+	infraMgrSvc               = "infra-mgr"
+	infraEditorSvc            = "infra-editor"
+	dashboardSvc              = "th2-infra-dashboard"
+	retries                   = 10
+	timeout                   = 5 * time.Second
 )
 
 var (
@@ -128,31 +125,6 @@ func TestRabbitMQRedirectEndpoint(t *testing.T) {
 	http_helper.HttpGetWithRetryWithCustomValidation(t, endpoint, nil, retries, timeout, validator)
 }
 
-func TestRabbitMQQueues(t *testing.T) {
-	// t.Parallel()
-	req, err := http.NewRequest("GET", "http://localhost:30000/rabbitmq/api/queues/th2-schema/link%5Bth2-schema%3Arpt-data-provider%3Afrom_codec%5D", nil)
-	if err != nil {
-		return false
-	}
-	req.SetBasicAuth(rabbitmqUser, rabbitmqPassword)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return false
-	}
-	defer resp.Body.Close()
-	str, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return false
-	}
-	if string(str) == `{"error":"Object Not Found","reason":"Not Found"}` {
-		return false
-	}
-	if string(str) != "" {
-		return true
-	}
-	return false
-}
-
 func TestInfraMgrEndpoint(t *testing.T) {
 	// t.Parallel()
 	endpoint := "http://localhost:30000/editor/backend/actuator/health"
@@ -180,5 +152,14 @@ func TestNamespaceDataProviderEndpoint(t *testing.T) {
 	k8s.WaitUntilServiceAvailable(t, options, dataProviderSvc, retries, timeout)
 
 	validator := validFunc(t, 200, "[]")
+	http_helper.HttpGetWithRetryWithCustomValidation(t, endpoint, nil, retries, timeout, validator)
+}
+
+func TestRabbitMQQueues(t *testing.T) {
+	// t.Parallel()
+	endpoint := fmt.Sprintf("http://%[1]s:%[2]slocalhost:30000/rabbitmq/api/queues/%[3]s/link%5B%[3]s%3Arpt-data-provider%3Afrom_codec%5D", rabbitmqUser, rabbitmqPassword, schemaNamespace)
+	options := k8s.NewKubectlOptions("", "", serviceNamespace)
+	k8s.WaitUntilPodAvailable(t, options, rabbitmqPod, retries, timeout)
+	validator := validFunc(t, 200, "{\"name\":\"link[th2-commonv3:act-custom-ext:from_codec]\"}")
 	http_helper.HttpGetWithRetryWithCustomValidation(t, endpoint, nil, retries, timeout, validator)
 }
