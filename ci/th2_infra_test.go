361f2e06ba55ee21ce1cf505432ce915390b4ee8
Dmitry Makhrovsky <690243+bop0hz@users.noreply.github.com>
2022-08-23 11:44:50 +0400

    typo fix

--- ci/th2_infra_test.go
+++ ci/th2_infra_test.go
@@ -192,7 +192,6 @@ func TestPodCommonAnnotationsOnly(t *testing.T) {
 func TestTH2Main(t *testing.T) {
 	// t.Parallel()
 	endpoint := "http://localhost:30000/"
-	options := k8s.NewKubectlOptions("", "", serviceNamespace)
 	validator := validFunc(t, 200, "<title>Welcome to th2</title>")
 	http_helper.HttpGetWithRetryWithCustomValidation(t, endpoint, nil, retries, timeout, validator)
 }
@@ -200,7 +199,6 @@ func TestTH2Main(t *testing.T) {
 func TestTH2MainAsset(t *testing.T) {
 	// t.Parallel()
 	endpoint := "http://localhost:30000/assets/js/axios.min.js"
-	options := k8s.NewKubectlOptions("", "", serviceNamespace)
 	validator := validFunc(t, 200, "/* axios v0.27.2 | (c) 2022 by Matt Zabriskie */")
 	http_helper.HttpGetWithRetryWithCustomValidation(t, endpoint, nil, retries, timeout, validator)
 }
