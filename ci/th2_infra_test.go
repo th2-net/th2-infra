4323461522e554db94563fe573cdadb3c866da55
sergey.e.smirnov <sergey.e.smirnov@exactprosystems.com>
2022-09-01 17:44:40 +0300

    fix timeout

--- ci/th2_infra_test.go
+++ ci/th2_infra_test.go
@@ -198,7 +198,7 @@ func TestCodecFixPod(t *testing.T) {
 	pods := k8s.ListPods(t, options, filters)
 	codecFixPodName := pods[0].ObjectMeta.Name
 	k8s.WaitUntilPodAvailable(t, options, codecFixPodName, retries, timeout)
-	time.Sleep(1 * time.Minutes)
+	time.Sleep(1 * time.Minute)
 }
 
 func TestTH2Main(t *testing.T) {
