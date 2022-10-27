13a3288730f96ed6975b23c149793c0924c265d1
getsadzeg <ggets20@freeuni.edu.ge>
2022-10-27 15:22:52 +0400

    trying to retrieve logs from the full last hour

--- ci/th2_infra_test.go
+++ ci/th2_infra_test.go
@@ -86,7 +86,7 @@ func TestErrorsInMgr(t *testing.T) {
 		k8s.WaitUntilServiceAvailable(t, options, appName, retries, timeout) //appName is the same as service name in our case
 		logs, err := shell.RunShellCommandAndGetOutput(
 			shell.NewShellOptions(),
-			"kubectl", "--namespace", serviceNamespace, "logs", "-l", "--since=1h", "app="+appName)
+			"kubectl", "--namespace", serviceNamespace, "logs", "--since=1h", "-l", "app="+appName)
 
 		return logs, err
 	}
